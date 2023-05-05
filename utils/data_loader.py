ab71ea5db9e3bb6f0a8c18bc99be4e0a93409cb9
Sebastian Ammon <sebastian@unetiq.com>
2023-05-05 17:38:33 +0200

    add dataloader utils

---
+++ utils/data_loader.py
@@ -0,0 +1,84 @@
+import pickle
+from pathlib import Path
+
+import numpy as np
+from scipy.spatial.distance import squareform, pdist
+from sklearn.model_selection import train_test_split
+
+
+class Data:
+    @staticmethod
+    def distance_matrix(node_coords):
+        return squareform(pdist(node_coords, metric='euclidean'))
+
+    @staticmethod
+    def target_matrix(num_nodes, routes):
+        adj_matrix = np.zeros((num_nodes, num_nodes))
+
+        for path in routes.values():
+            # start and end at depot
+            path = [0] + list(path) + [0]
+
+            for i in range(len(path) - 1):
+                xi, xj = path[i], path[i + 1]
+
+                adj_matrix[xi, xj] = 1
+                adj_matrix[xj, xi] = 1
+
+        # remove self connections
+        np.fill_diagonal(adj_matrix, 0)
+
+        return adj_matrix
+
+    @staticmethod
+    def edge_matrix(distance_matrix, k=3):
+        edge_matrix = np.zeros_like(distance_matrix)
+
+        # find k-nearst neighbors
+        for i in range(len(distance_matrix)):
+            knns = np.argsort(distance_matrix[i])[1:k + 1]
+            edge_matrix[i, knns] = 1  # neat trick
+
+        # self connections
+        np.fill_diagonal(edge_matrix, 2)
+
+        return edge_matrix
+
+    @classmethod
+    def prepare(cls, raw_dataset, k=3):
+        return [cls._process(instance, k=k) for instance in raw_dataset]
+
+    @classmethod
+    def _process(cls, instance, k=3):
+        node_demands, routes = instance
+
+        node_demands = np.array(node_demands)
+
+        coords = np.array(node_demands)[:, :2]
+        demand = np.array(node_demands)[:, 2]
+        distance_matrix = cls.distance_matrix(coords)
+        target_matrix = cls.target_matrix(len(coords), routes)
+        edge_matrix = cls.edge_matrix(distance_matrix, k=k)
+
+        return coords, demand, distance_matrix, edge_matrix, target_matrix
+
+    @classmethod
+    def load(cls, filename="vrp_11", test_size=0.2, random_state=42):
+        if filename.endswith(".pkl"):
+            raise ValueError("Filename must not end with .pkl")
+
+        data_dir = Path(__file__).parent.parent / "data"
+        with open(data_dir / f"{filename}.pkl", 'rb') as f:
+            instances = pickle.load(f)
+
+        with open(data_dir / f"{filename}_solved.pkl", 'rb') as f:
+            solutions = pickle.load(f)
+
+        assert len(instances) == len(solutions), "Number of instances and solutions must be equal"
+        assert np.all(
+            [solution['instance'] == i for i, solution in enumerate(solutions)]), "Instance numbers must be equal"
+
+        dataset = [(instance, solution['routes']) for instance, solution in zip(instances, solutions)]
+        train, test = train_test_split(dataset, test_size=test_size, random_state=random_state, shuffle=True)
+
+        return train, test
