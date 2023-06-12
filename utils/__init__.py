4442f0f6d5dbabed923a54eafe965fad7e8fe985
Sebastian Ammon <sebastian@unetiq.com>
2023-06-12 16:19:38 +0200

    update shortest valid tour

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
+from .beam_search import BeamSearch
 from .common import DotDict, _n, load_config, save_pickle, load_pickle
 from .model import get_device, load_checkpoint, save_checkpoint, get_metrics
